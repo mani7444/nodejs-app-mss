86bfca7ffa9a41e91f1848e20da4b3d8a66e8560
mani7444 <87255850+mani7444@users.noreply.github.com>
2021-11-22 10:23:48 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://10.15.50.196:9000/',
+  serverUrl: 'http://167.179.102.254:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
